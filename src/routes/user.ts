bc30730afecf59aaa2bdf9545daa466f2d59317a
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-02-13 15:32:38 +0100

    change renamed method in userController

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,35 +1,35 @@
 import { Router } from "express";
 import UserController from "../controller/UserController";
-import { checkJwt } from "../middlewares/checkJwt";
-import { checkRole } from "../middlewares/checkRole";
+import checkJwt  from "../middlewars/checkJwt";
+import checkRole from "../middlewars/checkRole";
 
 const router = Router();
 
 //Get all users
-router.get("/", [checkJwt, checkRole(["ADMIN"])], UserController.listAll);
+router.get("/", [checkJwt, checkRole(["ADMIN"])], UserController.index);
 
 // Get one user
 router.get(
   "/:id([0-9]+)",
   [checkJwt, checkRole(["ADMIN"])],
-  UserController.getOneById
+  UserController.show
 );
 
 //Create a new user
-router.post("/", [checkJwt, checkRole(["ADMIN"])], UserController.newUser);
+router.post("/", [checkJwt, checkRole(["ADMIN"])], UserController.create);
 
 //Edit one user
 router.patch(
   "/:id([0-9]+)",
   [checkJwt, checkRole(["ADMIN"])],
-  UserController.editUser
+  UserController.edit
 );
 
 //Delete one user
 router.delete(
   "/:id([0-9]+)",
   [checkJwt, checkRole(["ADMIN"])],
-  UserController.deleteUser
+  UserController.delete
 );
 
 export default router;

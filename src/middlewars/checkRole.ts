998ba8cd5c208522bd24d431090df54225a40bd5
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-02-14 17:38:53 +0100

    rewrite the middlewar array

--- src/middlewars/checkRole.ts
+++ src/middlewars/checkRole.ts
@@ -1,25 +1,35 @@
 import { Request, Response, NextFunction } from "express";
-import { getRepository } from "typeorm";
+import { getRepository, getConnection } from "typeorm";
 
 import { User } from "../entity/User";
+import { UserRole } from "../entity/UserRole";
 
 const checkRole = (roles: Array<string>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
+
     //Get the user ID from previous midleware
-    const id = res.locals.jwtPayload.userId;
+    const userId = res.locals.jwtPayload.userId;
 
     //Get user role from the database
-    const userRepository = getRepository(User);
+    const userRepository = await getConnection("mysqlDatabase").getRepository(User);
+    const roleRepository = await getConnection("mysqlDatabase").getRepository(UserRole);
     let user: User;
+    let userRole: UserRole;
     try {
-      user = await userRepository.findOneOrFail(id);
-    } catch (id) {
-      res.status(401).send();
+      user = await userRepository.findOneOrFail(userId);     
+      userRole = await roleRepository.findOne(user.role);
+      
+
+    } catch (error) {
+      res.status(401).send(error);
+
     }
 
-    //Check if array of authorized roles includes the user's role
-    if (roles.indexOf(user.role) > -1) next();
-    else res.status(401).send();
+      //Check if Array of authorized roles includes the user's role
+    if (roles.indexOf(`${userRole.name}`, 0) > -1) 
+      next();
+    else 
+      res.status(401).send();
   };
 };
 

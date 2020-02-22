862c629f780abc2937a131158c43a0f46b39841a
Abdelali EL BAHLOUL <bahloul97abdel@gmail.com>
2020-02-22 15:25:01 +0100

    feat(entity/user) : update relantionship

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -10,29 +10,33 @@ import {
     JoinColumn,
     OneToOne
   } from "typeorm";
-  import { Length, IsNotEmpty, IsEmpty } from "class-validator";
+  import { Length, IsNotEmpty, IsEmpty, IsEmail, IsInt } from "class-validator";
   import * as bcrypt from "bcryptjs";
 import { UserRole } from "./UserRole";
   
   @Entity()
-  @Unique(["username", "email"])
+  @Unique("unique keys",["userName", "email"])
   export class User {
     @PrimaryGeneratedColumn("increment")
     id: number;
   
     @Column()
+    @IsNotEmpty()
     @Length(4, 60)
     firstName: string;
 
     @Column()
+    @IsNotEmpty()
     @Length(4, 60)
     lastName: string;
 
     @Column()
+    @IsNotEmpty()
     @Length(4, 60)
     userName: string;
 
     @Column()
+    @IsEmail()
     @Length(4, 255)
     email: string;
   
@@ -40,8 +44,9 @@ import { UserRole } from "./UserRole";
     @Length(4, 255)
     password: string;
     
-    @OneToOne(type => UserRole)
+    @OneToOne(type => UserRole, role => role.id)
     @JoinColumn()
+    @IsInt()
     role: UserRole;
   
     @Column()
